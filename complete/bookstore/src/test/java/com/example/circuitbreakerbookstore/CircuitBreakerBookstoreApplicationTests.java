bc5f674160ebeb2bf93a9c2a6172eac1c347bcb3
Jay Bryant <jbryant@pivotal.io>
2019-10-22 12:47:51 -0500

    Upgraded to Spring Boot 2.2
    I upgraded to Spring Boot 2.2, which necessitated changing the tests to work with JUnit 5 and replacing the Gradle wrappers. I also caught some work I hadn't finished (package rearranging) from my Initializr pass. Everything still worked, but it was in half-moved state. I finalized that.

---
+++ complete/bookstore/src/test/java/com/example/circuitbreakerbookstore/CircuitBreakerBookstoreApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.circuitbreakerbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CircuitBreakerBookstoreApplicationTests {
+
+    @Autowired
+    private TestRestTemplate rest;
+
+    @Test
+    public void recommendedTest() {
+        String resp = rest.getForObject("/recommended", String.class);
+        assertThat(resp).isEqualTo("Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)");
+    }
+}
---
+++ complete/reading/src/test/java/com/example/circuitbreakerreading/BookServiceTests.java
@@ -0,0 +1,34 @@
+package com.example.circuitbreakerreading;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+@RestClientTest(BookService.class)
+public class BookServiceTests {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private MockRestServiceServer server;
+
+    @Test
+    public void readingListTest() {
+        this.server.expect(requestTo("http://localhost:8090/recommended"))
+                .andRespond(withSuccess("books", MediaType.TEXT_PLAIN));
+        assertThat(bookService.readingList()).isEqualTo("books");
+    }
+
+    @Test
+    public void reliable() {
+        assertThat(bookService.reliable()).isEqualTo("Cloud Native Java (O'Reilly)");
+    }
+}
